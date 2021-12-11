8a01bd5e9c638410c88f1a6a845ec156ed808714
Alex Kim <alex@ajpkim.com>
2021-12-10 19:20:57 -0500

    Updated and reduced emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -41,9 +41,9 @@
       ((agenda "" nil)
        (alltodo "" nil))
       nil)
-     ("v" "Active tasks" alltodo "ACTIVE" nil)) t)
+     ("v" "Active tasks" alltodo "ACTIVE" nil)))
  '(org-agenda-files
-   '("~/org/self.org" "~/org/poker.org" "~/org/inbox.org" "~/org/interwebs.org" "~/org/development.org" "~/org/library.org" "~/org/anki-exports.org" "~/org/anki.org" "~/org/learn.org" "~/org/habits.org" "~/.emacs.d/config.org" "/home/ajpkim/org/journal/2021-12-06.org"))
+   '("~/.emacs.d/new-config.org" "~/org/self.org" "~/org/poker.org" "~/org/inbox.org" "~/org/interwebs.org" "~/org/development.org" "~/org/library.org" "~/org/anki-exports.org" "~/org/anki.org" "~/org/learn.org" "~/org/habits.org" "~/.emacs.d/config.org" "/home/ajpkim/org/journal/2021-12-09.org"))
  '(package-selected-packages
    '(rainbow-delimiters ob-go go-mode simple-httpd ox-hugo lsp-ivy csharp-mode lsp-mode org-mime pass pinentry org-pomodoro magit elfeed ein company-quickhelp company-anaconda anaconda-mode company geiser-mit geiser pastelmac-theme clues-theme afternoon-theme sicp anki-editor multiple-cursors counsel ivy smartparens olivetti modus-themes org-journal org-roam toc-org htmlize org-drill drag-stuff ace-window rainbow-mode avy org-bullets beacon dracula-theme which-key use-package))
  '(pos-tip-background-color "#414E63")
