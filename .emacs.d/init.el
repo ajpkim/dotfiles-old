720d372befae1f44c20cf160b5dde56b5aa486be
Alex Kim <alex@ajpkim.com>
2022-07-18 01:52:17 -0400

    Update email config (mbsync, emacs) for fastmail migration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -44,7 +44,7 @@
       nil)
      ("v" "Active tasks" alltodo "ACTIVE" nil)))
  '(org-agenda-files
-   '("/home/ajpkim/org/career.org" "/home/ajpkim/org/personal.org" "/home/ajpkim/org/code.org" "/home/ajpkim/org/inbox.org" "/home/ajpkim/org/journal/2022-06-20.org"))
+   '("/home/ajpkim/org/career.org" "/home/ajpkim/org/personal.org" "/home/ajpkim/org/code.org" "/home/ajpkim/org/inbox.org" "/home/ajpkim/org/journal/2022-07-17.org"))
  '(package-selected-packages
    '(simple-modeline doom-modeline helpful ivy-rich auto-package-update projectile citar citeproc csv-mode erc-image erc-hl-nicks rainbow-delimiters ob-go go-mode simple-httpd ox-hugo lsp-ivy csharp-mode lsp-mode org-mime pass pinentry org-pomodoro magit elfeed ein company-quickhelp company-anaconda anaconda-mode company geiser-mit geiser pastelmac-theme clues-theme afternoon-theme sicp anki-editor multiple-cursors counsel ivy smartparens olivetti modus-themes org-journal org-roam toc-org htmlize org-drill drag-stuff ace-window rainbow-mode avy org-bullets beacon dracula-theme which-key use-package))
  '(pos-tip-background-color "#414E63")
